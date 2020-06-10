73433b3dad3535a639d5716254df39230ea42f8f
Grant <gmontgomery1994@gmail.com>
2020-06-09 22:59:59 -0700

    anvil logo works and loads correctly.

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,11 +1,12 @@
 import * as React from "react"
-import { FullLogo } from "../Logos"
+import { FullLogo, Anvil } from "../Logos"
 import css from "./Nav.module.css"
 
 export const Nav = () => {
   return (
     <div className={`navWrapper ${css.navWrapper}`}>
       <FullLogo location="navBar"></FullLogo>
+      {/* <Anvil location="navBar"></Anvil> */}
     </div>
   )
 }
