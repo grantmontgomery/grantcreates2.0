bed864ceb7f398a62d0fa4fa1665a414826c9238
Grant <gmontgomery1994@gmail.com>
2020-06-10 17:49:56 -0700

    favicon made and nav styling made with objektiv font.

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -5,23 +5,37 @@ import css from "./Nav.module.css"
 export const Nav = () => {
   return (
     <nav className={`navWrapper ${css.navWrapper}`}>
-      <FullLogo location="navBar"></FullLogo>
+      <a href="" className={`logoLink ${css.logoLink}`}>
+        <FullLogo location="navBar"></FullLogo>
+      </a>
       {/* <Anvil location="navBar"></Anvil> */}
       <div className={`linksWrapper ${css.linksWrapper}`}>
         <div className={`link ${css.link}`}>
-          <div className={`text ${css.text}`}>Creations</div>
+          <a href="">
+            <div className={`text ${css.text}`}>Creations</div>
+          </a>
         </div>
         <div className={`link ${css.link}`}>
-          <div className={`text ${css.text}`}>Tools</div>
+          <a href="">
+            <div className={`text ${css.text}`}>Tools</div>
+          </a>
         </div>
         <div className={`link ${css.link}`}>
-          <div className={`text ${css.text}`}>Resume</div>
+          <a href="">
+            <div className={`text ${css.text}`}>Resume</div>
+          </a>
         </div>
         <div className={`link ${css.link}`}>
-          <div className={`text ${css.text}`}>About</div>
+          <a href="">
+            {" "}
+            <div className={`text ${css.text}`}>About</div>
+          </a>
         </div>
         <div className={`link ${css.link}`}>
-          <div className={`text ${css.text}`}>Contact</div>
+          <a href="">
+            {" "}
+            <div className={`text ${css.text}`}>Contact</div>
+          </a>
         </div>
       </div>
     </nav>
