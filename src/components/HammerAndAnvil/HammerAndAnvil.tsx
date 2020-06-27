de8bd174df0e3dca5fc96cddbfb4aade37f37c9a
Grant <gmontgomery1994@gmail.com>
2020-06-26 17:52:23 -0700

    trying to load in gltf file.

--- src/components/HammerAndAnvil/HammerAndAnvil.tsx
+++ src/components/HammerAndAnvil/HammerAndAnvil.tsx
@@ -1,14 +1,39 @@
 import * as React from "react"
 import { BoxTest } from "../BoxTest"
-import { Canvas } from "react-three-fiber"
+import { Canvas, useThree, useLoader } from "react-three-fiber"
+import three from "three"
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader"
 import css from "./HammerAndAnvil.module.css"
 
 export const HammerAndAnvil = () => {
+  const hammerandanvil = require("./GrantCreates3D.gltf")
+
+  const object = useLoader(GLTFLoader, hammerandanvil)
+
   return (
-    <Canvas className={`${css.canvas}`}>
+    <Canvas
+      className={`${css.canvas}`}
+      onCreated={({ gl }) => {
+        gl.shadowMap.enabled = true
+        // gl.shadowMap.type = THREE.PCFSoftShadowMap
+      }}
+      camera={{ position: [1, 3, 5] }}
+    >
       <ambientLight></ambientLight>
       <pointLight position={[10, 10, 10]}></pointLight>
-      <BoxTest></BoxTest>
+
+      <perspectiveCamera
+        aspect={16 / 9}
+        fov={45}
+        near={1}
+        far={1000}
+      ></perspectiveCamera>
+      <React.Suspense fallback={<BoxTest></BoxTest>}>
+        <primitive object={object}></primitive>
+      </React.Suspense>
+      <scene autoUpdate={true} background={null} environment={null}></scene>
+      {/* <BoxTest></BoxTest> */}
     </Canvas>
   )
+  // const scene = new three.Scene(), camera = new three.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0,1)
 }
