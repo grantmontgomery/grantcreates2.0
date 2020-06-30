dd34c35eb71fecba499b8dd1d04f5d830b6fe06f
Grant <gmontgomery1994@gmail.com>
2020-06-29 17:49:53 -0700

    working on manipulating the mesh for react logo to prevent light absorption.

--- src/components/HammerAndAnvil/HammerAndAnvil2.tsx
+++ src/components/HammerAndAnvil/HammerAndAnvil2.tsx
@@ -19,24 +19,24 @@ const HammerAndAnvil = () => {
   renderer.setSize(window.innerWidth, window.innerHeight * 0.89)
   renderer.setClearColor(0x000000, 0)
   document.getElementById("canvas").appendChild(renderer.domElement)
-
   const controls = new OrbitControls(camera, renderer.domElement)
   controls.enableZoom = false
   controls.autoRotate = true
-
   controls.maxPolarAngle = 0
   controls.minPolarAngle = 0.85
-  console.log(controls)
   camera.position.z = 5
 
   const ambient = new THREE.AmbientLight(0x00000)
+
+  console.log(ambient)
 
   const directionalLight = new THREE.DirectionalLight(0xffffff, 2)
   directionalLight.position.set(0, 300, 500)
   directionalLight.castShadow = true
 
-  const point = new THREE.PointLight(0xfffff, 2)
-  point.position.set(5, 5, 5)
+  const point = new THREE.PointLight(0xffffff, 1.5)
+  console.log(point)
+  point.position.set(10, 10, 10)
 
   scene.add(point)
   scene.add(ambient)
@@ -47,7 +47,8 @@ const HammerAndAnvil = () => {
     "static/GrantCreates3DGLB.glb",
     function (gltf) {
       scene.add(gltf.scene)
-      console.log(gltf.scene.children)
+
+      console.log(gltf.scene.children[0].children[0])
 
       gltf.scene.children[0].scale.x = 0.03
       gltf.scene.children[0].scale.y = 0.03
