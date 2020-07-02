19ab635a349e51a853788c1782d7e729578f12bb
Grant <gmontgomery1994@gmail.com>
2020-07-01 18:14:12 -0700

    still trying to implement typing effect.

--- src/components/Headline/Headline.tsx
+++ src/components/Headline/Headline.tsx
@@ -1,27 +1,28 @@
 import * as React from "react"
+import { Typical } from "react-typical"
 import css from "./Headline.module.css"
 
-const type = (creations: string[]) => {
-  let count = 0
-  let index = 0
-  let currentText = ""
-  let letter = ""
+// const type = (creations: string[]) => {
+//   let count = 0
+//   let index = 0
+//   let currentText = ""
+//   let letter = ""
 
-  if (count === creations.length) {
-    count = 0
-  }
+//   if (count === creations.length) {
+//     count = 0
+//   }
 
-  currentText = creations[count]
-  letter = currentText.slice(0, ++index)
-  document.querySelector(`.${css.dynamicText}`).textContent = letter
-  if (letter.length === currentText.length) {
-    count++
-    index = 0
-  }
-  setTimeout(() => {
-    type(creations)
-  }, 400)
-}
+//   currentText = creations[count]
+//   letter = currentText.slice(0, ++index)
+//   document.querySelector(`.${css.dynamicText}`).textContent = letter
+//   if (letter.length === currentText.length) {
+//     count++
+//     index = 0
+//   }
+//   setTimeout(() => {
+//     type(creations)
+//   }, 400)
+// }
 
 export const Headline = () => {
   const creations: string[] = [
@@ -31,13 +32,27 @@ export const Headline = () => {
     "SOLUTIONS",
   ]
 
-  React.useEffect(() => type(creations), [])
+  //   React.useEffect(() => type(creations), [])
 
   return (
     <div className={`${css.headlineWrapper}`}>
       <div className={`${css.mainHeadline}`}>
-        <h1 className={`${css.staticText}`}>I CREATE</h1>
-        <h1 className={`${css.dynamicText}`}></h1>
+        <h1 className={`${css.staticText}`}>
+          I CREATE{" "}
+          {/* <Typical
+            loop={Infinity}
+            steps={[
+              "APPLICATIONS",
+              1000,
+              "WEBSITES",
+              1000,
+              "DESIGNS",
+              1000,
+              "SOLUTIONS",
+              1000,
+            ]}
+          ></Typical> */}
+        </h1>
       </div>
     </div>
   )
