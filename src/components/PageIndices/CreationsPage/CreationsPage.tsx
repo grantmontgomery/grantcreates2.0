c98ef0140e5d18e1eda440f34aac2743a69f47d5
Grant <gmontgomery1994@gmail.com>
2020-07-06 23:20:42 -0700

    effect for creation types set. and resized hammer and anvil object.

--- src/components/PageIndices/CreationsPage/CreationsPage.tsx
+++ src/components/PageIndices/CreationsPage/CreationsPage.tsx
@@ -18,25 +18,31 @@ export const CreationsPage = ({ location }: Props) => {
       </div>
       <ul className={`${css.creationTypes}`}>
         <li
-          className={`${css.type}`}
+          className={`${css.type} ${state.type === "Apps" ? css.active : null}`}
           onClick={() => setState({ type: "Apps" })}
         >
           <span>Apps</span>
         </li>
         <li
-          className={`${css.type}`}
+          className={`${css.type} ${
+            state.type === "Websites" ? css.active : null
+          }`}
           onClick={() => setState({ type: "Websites" })}
         >
           <span>Websites</span>
         </li>
         <li
-          className={`${css.type}`}
+          className={`${css.type} ${
+            state.type === "Designs" ? css.active : null
+          }`}
           onClick={() => setState({ type: "Designs" })}
         >
           <span>Designs</span>
         </li>
         <li
-          className={`${css.type}`}
+          className={`${css.type} ${
+            state.type === "Experiences" ? css.active : null
+          }`}
           onClick={() => setState({ type: "Experiences" })}
         >
           <span>Experiences</span>
