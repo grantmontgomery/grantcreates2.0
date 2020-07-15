c55ff5e294975820898a3c202a51f808f119859b
Grant <gmontgomery1994@gmail.com>
2020-07-14 17:32:56 -0700

    more form styling and setup.

--- src/components/ContactFloat/Parts/SenderForm/SenderForm.tsx
+++ src/components/ContactFloat/Parts/SenderForm/SenderForm.tsx
@@ -4,14 +4,51 @@ import css from "./SenderForm.module.css"
 export const SenderForm = () => {
   return (
     <div className={css.senderWrapper}>
-      <div className={css.nameWrapper}>
-        <label htmlFor="">Name</label>
-        <input type="text" />
-        <div className={css.errorWrapper}></div>
+      <div className={css.sectionWrapper}>
+        <div className={css.labelWrapper}>
+          <label htmlFor="">Name</label>
+        </div>
+        <div className={css.inputWrapper}>
+          <input type="text" />
+        </div>
+        <div className={css.errorWrapper}>
+          <div className={css.error}>No empty fields.</div>
+        </div>
       </div>
-      <div className={css.companyWrapper}></div>
-      <div className={css.emailWrapper}></div>
-      <div className={css.phoneWrapper}></div>
+
+      <div className={css.sectionWrapper}>
+        <div className={css.labelWrapper}>
+          <label htmlFor="">Company</label>
+        </div>
+        <div className={css.inputWrapper}>
+          <input type="text" placeholder="(Optional)" />
+        </div>
+        <div className={css.errorWrapper}>
+          <div className={css.error}></div>
+        </div>
+      </div>
+      <div className={css.sectionWrapper}>
+        <div className={css.labelWrapper}>
+          <label htmlFor="">Email</label>
+        </div>
+        <div className={css.inputWrapper}>
+          <input type="text" />
+        </div>
+        <div className={css.errorWrapper}>
+          <div className={css.error}></div>
+        </div>
+      </div>
+      <div className={css.sectionWrapper}>
+        <div className={css.labelWrapper}>
+          <label htmlFor="">Phone</label>
+        </div>
+        <div className={css.inputWrapper}>
+          <input type="text" />
+        </div>
+        <div className={css.errorWrapper}>
+          <div className={css.error}></div>
+        </div>
+      </div>
     </div>
   )
 }
