79a360a6e8c92653e5dd1a97a7ad28df9908b1fb
Grant <grantmontgomery1994@gmail.com>
2020-07-19 13:03:01 -0700

    nodemailer finally works and is sending emails to my gmail.

--- src/redux/wrapper.jsx
+++ src/redux/wrapper.jsx
@@ -7,8 +7,8 @@ import { Provider } from "react-redux"
 const store = createStore(
   allReducers,
   compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(thunk)
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 )
 
--- src/server/server.js
+++ src/server/server.js
@@ -1,5 +1,6 @@
 const bodyParser = require("body-parser")
 const cors = require("cors")
+require(`dotenv`).config()
 
 const express = require("express")
 
@@ -12,8 +13,6 @@ const nodemailer = require("nodemailer")
 
 const app = express()
 
-console.log(process.env.GATSBY_BROWSER)
-
 app.use(cors())
 
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -24,8 +23,61 @@ app.use(bodyParser.json())
 //   res.send("<h1>Hello World</h1>")
 // })
 
-app.post("/send", (req, res) => {
-  res.send(console.log(req.body))
-})
+app.post(
+  "/send",
+  ({ body: { name, phone, email, subject, message, company } }, res) => {
+    const output = `
+  <p>You have a new contact request</p>
+  <h3>Contact Details</h3>
+  <ul>
+  <li>Name: ${name} </li>
+  <li>Company: ${company} </li>
+  <li>Phone: ${phone} </li>
+  <li>Email: ${email} </li>
+  <li>Subject: ${subject} </li>
+  <li>Message: ${message} </li>
+  </ul>
+  `
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+      service: `gmail`,
+      host: `smtp.gmail.com`,
+      port: 587,
+      secure: false, // true for 465, false for other ports
+      auth: {
+        user: `nodetest69@gmail.com`, // generated ethereal user
+        pass: "bruh", // fake password.
+      },
+      tls: {
+        rejectUnauthorized: false,
+      },
+    })
+
+    const mailoptions = {
+      from: `"Nodemailer Contact" <nodetest69@gmail.com>`,
+      to: `nodetest69@gmail.com`,
+      subject: `Node Contact Request`,
+      text: `Hello World?`,
+      html: output,
+    }
+
+    // send mail with defined transport object
+    // let info = await transporter.sendMail({
+    //   from: '"Fred Foo 👻" <foo@example.com>', // sender address
+    //   to: "bar@example.com, baz@example.com", // list of receivers
+    //   subject: "Hello ✔", // Subject line
+    //   text: "Hello world?", // plain text body
+    //   html: "<b>Hello world?</b>", // html body
+    // });
+
+    transporter.sendMail(mailoptions, (error, info) => {
+      if (error) {
+        return console.log(error)
+      }
+
+      console.log(`Message sent`, info.messageId)
+    })
+  }
+)
 
 app.listen(5000, () => console.log("Server started on port 5000"))
