b3bb2c047617263f87330624b4256e2e5020b165
Grant <gmontgomery1994@gmail.com>
2020-07-27 22:21:30 -0700

    things look like they're back to normal on site. Going to stick with using the svg version of the mail logo instead of the image version.

--- src/components/PageIndices/AboutPage/AboutPage.tsx
+++ src/components/PageIndices/AboutPage/AboutPage.tsx
@@ -6,6 +6,23 @@ type Props = {
 }
 
 export const AboutPage: React.FC<Props> = ({ aboutRef }) => {
+  //For When I need to drop in a picture of myself.
+  // const { file } = useStaticQuery(graphql`
+  //   query NewQuery {
+  //     file(relativePath: { eq: "MailIconCompressed.png" }) {
+  //       childImageSharp {
+  //         fluid {
+  //           aspectRatio
+  //           base64
+  //           sizes
+  //           src
+  //           srcSet
+  //         }
+  //       }
+  //     }
+  //   }
+  // `)
+
   return (
     <section ref={aboutRef} id="about" className={css.about}>
       <div className={css.headerWrapper}>
