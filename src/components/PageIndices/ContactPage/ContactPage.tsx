ba3ad75abf62beab08893a87b2af75b5f216fc38
Grant <gmontgomery1994@gmail.com>
2020-07-30 21:51:19 -0700

    contact set up.

--- src/components/PageIndices/ContactPage/ContactPage.tsx
+++ src/components/PageIndices/ContactPage/ContactPage.tsx
@@ -7,10 +7,11 @@ type Props = {
 
 export const ContactPage: React.FC<Props> = ({ contactRef }) => {
   return (
-    <section
-      id="contact"
-      ref={contactRef}
-      className={`${css.contact}`}
-    ></section>
+    <section id="contact" ref={contactRef} className={css.contact}>
+      <span className={css.header}>Contact</span>
+      <span className={css.email}>grant@grantcreates.com</span>
+      <button className={css.hireMe}>Hire Me</button>
+      <div className={css.socialLinks}></div>
+    </section>
   )
 }
