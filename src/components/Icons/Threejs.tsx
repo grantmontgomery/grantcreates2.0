84fe8e8b4f80a2673268b52649da2dee5db3bcf6
Grant <gmontgomery1994@gmail.com>
2020-08-01 23:04:23 -0700

    small issue with returning right SVGIcon.

--- src/components/Icons/Threejs.tsx
+++ src/components/Icons/Threejs.tsx
@@ -1,28 +1,118 @@
-import * as React from 'react';
+import * as React from "react"
 
-export const Threejs:React.FC = () => {
-    return <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-         viewBox="0 0 570 570" style="enable-background:new 0 0 570 570;" xml:space="preserve">
-    <style type="text/css">
+export const Threejs: React.FC = () => {
+  return (
+    <svg version="1.1" id="Layer_1" x="0px" y="0px" viewBox="0 0 570 570">
+      {/* <style type="text/css">
         .st0{fill:none;stroke:#000000;stroke-width:5;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:10;}
-    </style>
-    <g>
-        <polygon class="st0" points="322.45,220.13 359.91,359.91 462.23,257.58 	"/>
-        <polygon class="st0" points="220.13,322.45 257.58,462.23 359.91,359.91 	"/>
-        <polygon class="st0" points="80.35,285 117.81,424.78 220.13,322.45 	"/>
-        <polygon class="st0" points="42.9,145.22 80.35,285 182.68,182.68 	"/>
-        <polygon class="st0" points="145.22,42.9 182.68,182.68 285,80.35 	"/>
-        <polygon class="st0" points="285,80.35 322.45,220.13 424.78,117.81 	"/>
-        <polygon class="st0" points="5.45,5.45 42.9,145.22 145.22,42.9 	"/>
-        <polygon class="st0" points="117.81,424.78 155.26,564.55 257.58,462.23 	"/>
-        <polygon class="st0" points="220.13,322.45 117.81,424.78 257.58,462.23 	"/>
-        <polygon class="st0" points="182.68,182.68 80.35,285 220.13,322.45 	"/>
-        <polygon class="st0" points="285,80.35 182.68,182.68 322.45,220.13 	"/>
-        <polygon class="st0" points="145.22,42.9 42.9,145.22 182.68,182.68 	"/>
-        <polygon class="st0" points="322.45,220.13 220.13,322.45 359.91,359.91 	"/>
-        <polygon class="st0" points="424.78,117.81 322.45,220.13 462.23,257.58 	"/>
-        <polygon class="st0" points="424.78,117.81 462.23,257.58 564.55,155.26 	"/>
-    </g>
+    </style> */}
+      <g>
+        <polygon
+          strokeWidth="5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeMiterlimit="10"
+          points="322.45,220.13 359.91,359.91 462.23,257.58 	"
+        />
+        <polygon
+          strokeWidth="5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeMiterlimit="10"
+          points="220.13,322.45 257.58,462.23 359.91,359.91 	"
+        />
+        <polygon
+          strokeWidth="5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeMiterlimit="10"
+          points="80.35,285 117.81,424.78 220.13,322.45 	"
+        />
+        <polygon
+          strokeWidth="5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeMiterlimit="10"
+          points="42.9,145.22 80.35,285 182.68,182.68 	"
+        />
+        <polygon
+          strokeWidth="5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeMiterlimit="10"
+          points="145.22,42.9 182.68,182.68 285,80.35 	"
+        />
+        <polygon
+          strokeWidth="5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeMiterlimit="10"
+          points="285,80.35 322.45,220.13 424.78,117.81 	"
+        />
+        <polygon
+          strokeWidth="5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeMiterlimit="10"
+          points="5.45,5.45 42.9,145.22 145.22,42.9 	"
+        />
+        <polygon
+          strokeWidth="5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeMiterlimit="10"
+          points="117.81,424.78 155.26,564.55 257.58,462.23 	"
+        />
+        <polygon
+          strokeWidth="5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeMiterlimit="10"
+          points="220.13,322.45 117.81,424.78 257.58,462.23 	"
+        />
+        <polygon
+          strokeWidth="5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeMiterlimit="10"
+          points="182.68,182.68 80.35,285 220.13,322.45 	"
+        />
+        <polygon
+          strokeWidth="5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeMiterlimit="10"
+          points="285,80.35 182.68,182.68 322.45,220.13 	"
+        />
+        <polygon
+          strokeWidth="5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeMiterlimit="10"
+          points="145.22,42.9 42.9,145.22 182.68,182.68 	"
+        />
+        <polygon
+          strokeWidth="5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeMiterlimit="10"
+          points="322.45,220.13 220.13,322.45 359.91,359.91 	"
+        />
+        <polygon
+          strokeWidth="5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeMiterlimit="10"
+          points="424.78,117.81 322.45,220.13 462.23,257.58 	"
+        />
+        <polygon
+          strokeWidth="5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeMiterlimit="10"
+          points="424.78,117.81 462.23,257.58 564.55,155.26 	"
+        />
+      </g>
     </svg>
-    
+  )
 }
