a33d04b4e5e82b53f84a0dcae157046d039799b3
Grant <gmontgomery1994@gmail.com>
2020-08-02 15:25:29 -0700

    better mobilenav.

--- src/components/MobileNav/MobileNav.tsx
+++ src/components/MobileNav/MobileNav.tsx
@@ -7,17 +7,34 @@ type Props = {
 }
 
 export const MobileNav: React.FC<Props> = ({ navLinks, handleTap }) => {
-  const alterMenu = () => {
+  const alterMenu: () => string = () => {
     return navLinks ? "switched" : "normal"
   }
 
   return (
-    <div className={`${css.mobileNavWrapper} `} onClick={handleTap}>
-      <div className={`${css.animateFrame} ${css[`${alterMenu()}`]}`}>
-        <div className={`${css.line1}`}></div>
-        <div className={`${css.line2}`}></div>
-        <div className={`${css.line3}`}></div>
-      </div>
-    </div>
+    <svg
+      className={`${css.mobileNav} ${css[alterMenu()]}`}
+      onClick={handleTap}
+      viewBox="0 0 150 130"
+    >
+      <path
+        className={css.path1}
+        strokeWidth="1vh"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        fill="none"
+        stroke="white"
+        d="M19.64,120.75L127.5,12.89c6.77-6.77,18.36-1.98,18.36,7.6v0c0,5.94-4.81,10.75-10.75,10.75H4.14"
+      />
+      <path
+        className={css.path2}
+        strokeWidth="1vh"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        fill="none"
+        stroke="white"
+        d="M19.14,9.25L127,117.11c6.96,6.96,18.86,2.03,18.86-7.81v0c0-6.1-4.95-11.05-11.05-11.05H44.14"
+      />
+    </svg>
   )
 }
--- src/components/Tools/Tool/Tool.tsx
+++ src/components/Tools/Tool/Tool.tsx
@@ -5,36 +5,26 @@ type Props = {
   name: string
 }
 
+type SVGFunction = () => SVGElement
+
 export const Tool: React.FC<Props> = ({ name }) => {
-  const displaySVG: () => SVGElement = () => {
+  const displaySVG: SVGFunction = () => {
     if (name === "React") {
       const react: {
-        [key: string]: () => SVGElement
+        [key: string]: SVGFunction
       } = require("../../Icons/ReactIcon")
       return react.ReactIcon()
     } else {
       const filteredName = name.replace(/[\W_]+/g, "")
 
       const svgElem: {
-        [key: string]: () => SVGElement
+        [key: string]: SVGFunction
       } = require(`../../Icons/${filteredName}`)
       return svgElem[`${filteredName}`]()
     }
   }
-
-  const react = require("../../Icons/ReactIcon")
-
-  console.log(react.ReactIcon())
   return (
     <div className={css.toolWrapper}>
-      {/* <svg
-        role="img"
-        fill="white"
-        viewBox="0 0 24 24"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z" />
-      </svg> */}
       {displaySVG()}
       <p>{name}</p>
     </div>
