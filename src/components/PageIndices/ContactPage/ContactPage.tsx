989d9ee3c95356d71887618e3e0f40e182eadde3
Grant <gmontgomery1994@gmail.com>
2020-08-03 12:31:53 -0700

    hire me button tweaks.

--- src/components/PageIndices/ContactPage/ContactPage.tsx
+++ src/components/PageIndices/ContactPage/ContactPage.tsx
@@ -21,43 +21,7 @@ export const ContactPage: React.FC<Props> = ({ contactRef }) => {
   return (
     <section id="contact" ref={contactRef} className={css.contact}>
       <span className={css.header}>Contact</span>
-      <div className={css.contact}>
-        {/* <svg
-          version="1.1"
-          x="0px"
-          y="0px"
-          viewBox="0 0 905 605"
-          enableBackground="new 0 0 905 605"
-        >
-          <polygon
-            fill="none"
-            stroke="white"
-            strokeWidth="3vh"
-            points="902.49,602.49 2.49,602.49 327.3,277.68 452.5,383.76 577.69,277.69 		"
-          />
-
-          <polygon
-            fill="none"
-            stroke="white"
-            strokeWidth="3vh"
-            points="902.49,2.49 902.49,602.49 577.69,277.69 		"
-          />
-
-          <polygon
-            stroke="white"
-            strokeWidth="3vh"
-            fill="none"
-            points="327.3,277.68 2.49,602.49 2.49,2.49 		"
-          />
-          <polygon
-            stroke="white"
-            strokeWidth="3vh"
-            fill="none"
-            points="902.49,2.49 577.69,277.69 452.5,383.76 327.3,277.68 2.49,2.49 	"
-          />
-        </svg> */}
-        <span>grant@grantcreates.com</span>
-      </div>
+      <span className={css.contact}>grant@grantcreates.com</span>
       <button className={css.hireMe} onClick={handleTap}>
         Hire Me
       </button>
