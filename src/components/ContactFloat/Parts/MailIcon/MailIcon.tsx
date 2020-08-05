4e534ea3ced17e8b7af543d6184ef1e9871a1f8b
Grant <gmontgomery1994@gmail.com>
2020-08-05 14:51:46 -0700

    mail icon functions and sizes better.

--- src/components/ContactFloat/Parts/MailIcon/MailIcon.tsx
+++ src/components/ContactFloat/Parts/MailIcon/MailIcon.tsx
@@ -7,112 +7,112 @@ type Props = {
 
 export const MailIcon: React.FC<Props> = ({ handleTap }) => {
   return (
-    <div className={css.iconWrapper} onClick={handleTap}>
-      <svg
-        version="1.1"
-        x="0px"
-        y="0px"
-        viewBox="0 0 905 605"
-        enableBackground="new 0 0 905 605"
+    <svg
+      version="1.1"
+      x="0px"
+      y="0px"
+      viewBox="0 0 905 605"
+      enableBackground="new 0 0 905 605"
+      className={css.iconWrapper}
+      onClick={handleTap}
+    >
+      <defs>
+        <filter id="topFold">
+          <feDropShadow dx="0.2" dy="0.4" stdDeviation="0.2" />
+        </filter>
+        <filter id="leftFold">
+          <feDropShadow dx="0" dy="0" stdDeviation="0.5" floodColor="cyan" />
+        </filter>
+        <filter id="rightFold">
+          <feDropShadow
+            dx="-0.8"
+            dy="-0.8"
+            stdDeviation="0"
+            floodColor="pink"
+            floodOpacity="0.5"
+          />
+        </filter>
+        <filter id="bottomFold">
+          <feDropShadow
+            dx="-0.8"
+            dy="-0.8"
+            stdDeviation="0"
+            floodColor="pink"
+            floodOpacity="0.5"
+          />
+        </filter>
+      </defs>
+      <linearGradient
+        id="mailGradient1"
+        gradientUnits="userSpaceOnUse"
+        x1="742.5261"
+        y1="158.9535"
+        x2="725.5759"
+        y2="1976.0139"
       >
-        <defs>
-          <filter id="topFold">
-            <feDropShadow dx="0.2" dy="0.4" stdDeviation="0.2" />
-          </filter>
-          <filter id="leftFold">
-            <feDropShadow dx="0" dy="0" stdDeviation="0.5" floodColor="cyan" />
-          </filter>
-          <filter id="rightFold">
-            <feDropShadow
-              dx="-0.8"
-              dy="-0.8"
-              stdDeviation="0"
-              floodColor="pink"
-              floodOpacity="0.5"
-            />
-          </filter>
-          <filter id="bottomFold">
-            <feDropShadow
-              dx="-0.8"
-              dy="-0.8"
-              stdDeviation="0"
-              floodColor="pink"
-              floodOpacity="0.5"
-            />
-          </filter>
-        </defs>
-        <linearGradient
-          id="mailGradient1"
-          gradientUnits="userSpaceOnUse"
-          x1="742.5261"
-          y1="158.9535"
-          x2="725.5759"
-          y2="1976.0139"
-        >
-          <stop offset="0" stopColor="#FFFFFF" />
-          <stop offset="0.1764" stopColor="#FAFAFA" />
-          <stop offset="0.3854" stopColor="#ECECEC" />
-          <stop offset="0.6108" stopColor="#D4D4D4" />
-          <stop offset="0.8462" stopColor="#B3B3B3" />
-          <stop offset="1" stopColor="#999999" />
-        </linearGradient>
-        <line
-          className={`${css.st2}`}
-          x1="578.46"
-          y1="278.46"
-          x2="901.35"
-          y2="601.35"
-          strokeWidth="4px"
-        />
-        <line
-          className={`${css.st146}`}
-          x1="3.96"
-          y1="601.02"
-          x2="323.87"
-          y2="281.11"
-          strokeWidth="4px"
-        />
-        <line
-          className={`${css.st146}`}
-          x1="455.43"
-          y1="381.28"
-          x2="898.71"
-          strokeWidth="4px"
-          y2="5.7"
-        />
-        <line
-          className={`${css.st146}`}
-          x1="452.5"
-          y1="383.76"
-          x2="4.7"
-          strokeWidth="4px"
-          y2="4.36"
-        />
+        <stop offset="0" stopColor="#FFFFFF" />
+        <stop offset="0.1764" stopColor="#FAFAFA" />
+        <stop offset="0.3854" stopColor="#ECECEC" />
+        <stop offset="0.6108" stopColor="#D4D4D4" />
+        <stop offset="0.8462" stopColor="#B3B3B3" />
+        <stop offset="1" stopColor="#999999" />
+      </linearGradient>
+      <line
+        className={`${css.st2}`}
+        x1="578.46"
+        y1="278.46"
+        x2="901.35"
+        y2="601.35"
+        strokeWidth="4px"
+      />
+      <line
+        className={`${css.st146}`}
+        x1="3.96"
+        y1="601.02"
+        x2="323.87"
+        y2="281.11"
+        strokeWidth="4px"
+      />
+      <line
+        className={`${css.st146}`}
+        x1="455.43"
+        y1="381.28"
+        x2="898.71"
+        strokeWidth="4px"
+        y2="5.7"
+      />
+      <line
+        className={`${css.st146}`}
+        x1="452.5"
+        y1="383.76"
+        x2="4.7"
+        strokeWidth="4px"
+        y2="4.36"
+      />
 
-        <polygon
-          //   className={`${css.st8}`}
-          className={css.topFold}
-          filter={`url(#topFold)`}
-          points="902.49,602.49 2.49,602.49 327.3,277.68 452.5,383.76 577.69,277.69 		"
-        />
+      <polygon
+        //   className={`${css.st8}`}
+        className={css.topFold}
+        filter={`url(#topFold)`}
+        points="902.49,602.49 2.49,602.49 327.3,277.68 452.5,383.76 577.69,277.69 		"
+      />
 
-        <polygon
-          className={`${css.rightFold}`}
-          filter="url(#rightFold)"
-          points="902.49,2.49 902.49,602.49 577.69,277.69 		"
-        />
+      <polygon
+        className={`${css.rightFold}`}
+        filter="url(#rightFold)"
+        points="902.49,2.49 902.49,602.49 577.69,277.69 		"
+      />
 
-        <polygon
-          className={css.leftFold}
-          filter="url(#leftFold)"
-          points="327.3,277.68 2.49,602.49 2.49,2.49 		"
-        />
-        <polygon
-          className={css.bottomFold}
-          filter="url(#bottomFold)"
-          points="902.49,2.49 577.69,277.69 452.5,383.76 327.3,277.68 2.49,2.49 	"
-        />
-      </svg>
-    </div>
+      <polygon
+        className={css.leftFold}
+        filter="url(#leftFold)"
+        points="327.3,277.68 2.49,602.49 2.49,2.49 		"
+      />
+      <polygon
+        className={css.bottomFold}
+        filter="url(#bottomFold)"
+        points="902.49,2.49 577.69,277.69 452.5,383.76 327.3,277.68 2.49,2.49 	"
+      />
+    </svg>
   )
 }
