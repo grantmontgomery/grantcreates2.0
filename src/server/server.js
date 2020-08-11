b5abcd5dee31fa71d3a0abfb252b14f20aa5a670
Grant <gmontgomery1994@gmail.com>
2020-08-11 14:38:54 -0700

    netlify lambda function works now.

--- src/server/server.js
+++ src/server/server.js
@@ -1,81 +1,99 @@
-// const gatsbyExpress = require("gatsby-plugin-express")
-const bodyParser = require("body-parser")
-const cors = require("cors")
-require(`dotenv`).config()
+// // const gatsbyExpress = require("gatsby-plugin-express")
+// const bodyParser = require("body-parser")
+// const cors = require("cors")
+// require(`dotenv`).config()
+
+// const express = require("express")
+
+// const nodemailer = require("nodemailer")
+// //transporter: the object that is able to send the email
+// //transport: the transport config object, connection url or a transport plugin instance
+// //defaults an object defines default values for mail options
+
+// // const transporter = nodemailer.createTransport()
+
+// const app = express()
+
+// app.use(
+//   cors({
+//     origin: `https://grantcreates.netlify.app`,
+//   })
+// )
+
+// app.use(bodyParser.urlencoded({ extended: false }))
+// app.use(bodyParser.json())
+
+// // app.use(cors())
+// // app.get("/send", (req, res) => {
+// //   res.send("<h1>Hello World</h1>")
+// // })
+
+// app.post(
+//   "/send",
+//   ({ body: { name, phone, email, subject, message, company } }, res) => {
+//     const output = `
+//   <p>A new visitor reached out!</p>
+//   <h3>Message Details</h3>
+//   <ul>
+//   <li>Name: ${name} </li>
+//   <li>Company: ${company} </li>
+//   <li>Phone: ${phone} </li>
+//   <li>Email: ${email} </li>
+//   <li>Subject: ${subject} </li>
+//   <li>Message: ${message} </li>
+//   </ul>
+//   `
+//     // `${process.env.GATSBY_EMAIL_SENDER_PASSWORD}`
+//     // create reusable transporter object using the default SMTP transport
+//     let transporter = nodemailer.createTransport({
+//       service: `gmail`,
+//       host: `smtp.gmail.com`,
+//       port: 587,
+//       secure: false, // true for 465, false for other ports
+//       auth: {
+//         user: `${process.env.GATSBY_EMAIL_SENDER}`, // generated ethereal user
+//         pass: `${process.env.GATSBY_EMAIL_SENDER_PASSWORD}`, // fake password.
+//       },
+//       tls: {
+//         rejectUnauthorized: false,
+//       },
+//     })
+
+//     const mailoptions = {
+//       from: `"Portfolio Visitor"<nodetest69@gmail.com>`,
+//       to: `grant@grantcreates.com`,
+//       subject: `Visitor Contact Request`,
+//       text: `Hello World?`,
+//       html: output,
+//     }
+
+//     transporter.sendMail(mailoptions, (error, info) => {
+//       if (error) {
+//         return console.log(error), res.send(error)
+//       }
+
+//       console.log(`Message sent`, info.messageId)
+//       res.send(info)
+//     })
+//   }
+// )
+
+// app.listen(5000, () => console.log("Server started on port 5000"))
 
 const express = require("express")
 
-const nodemailer = require("nodemailer")
-//transporter: the object that is able to send the email
-//transport: the transport config object, connection url or a transport plugin instance
-//defaults an object defines default values for mail options
-
-// const transporter = nodemailer.createTransport()
+const serverless = require("serverless-http")
 
 const app = express()
 
-app.use(
-  cors({
-    origin: `https://grantcreates.netlify.app`,
+const router = express.Router()
+
+router.get("/", (req, res) => {
+  res.json({
+    hello: "hi",
   })
-)
+})
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use("/.netlify/functions/server", router)
 
-// app.use(cors())
-// app.get("/send", (req, res) => {
-//   res.send("<h1>Hello World</h1>")
-// })
-
-app.post(
-  "/send",
-  ({ body: { name, phone, email, subject, message, company } }, res) => {
-    const output = `
-  <p>A new visitor reached out!</p>
-  <h3>Message Details</h3>
-  <ul>
-  <li>Name: ${name} </li>
-  <li>Company: ${company} </li>
-  <li>Phone: ${phone} </li>
-  <li>Email: ${email} </li>
-  <li>Subject: ${subject} </li>
-  <li>Message: ${message} </li>
-  </ul>
-  `
-    // `${process.env.GATSBY_EMAIL_SENDER_PASSWORD}`
-    // create reusable transporter object using the default SMTP transport
-    let transporter = nodemailer.createTransport({
-      service: `gmail`,
-      host: `smtp.gmail.com`,
-      port: 587,
-      secure: false, // true for 465, false for other ports
-      auth: {
-        user: `${process.env.GATSBY_EMAIL_SENDER}`, // generated ethereal user
-        pass: `${process.env.GATSBY_EMAIL_SENDER_PASSWORD}`, // fake password.
-      },
-      tls: {
-        rejectUnauthorized: false,
-      },
-    })
-
-    const mailoptions = {
-      from: `"Portfolio Visitor"<nodetest69@gmail.com>`,
-      to: `grant@grantcreates.com`,
-      subject: `Visitor Contact Request`,
-      text: `Hello World?`,
-      html: output,
-    }
-
-    transporter.sendMail(mailoptions, (error, info) => {
-      if (error) {
-        return console.log(error), res.send(error)
-      }
-
-      console.log(`Message sent`, info.messageId)
-      res.send(info)
-    })
-  }
-)
-
-app.listen(5000, () => console.log("Server started on port 5000"))
+module.exports.handler = serverless(app)
