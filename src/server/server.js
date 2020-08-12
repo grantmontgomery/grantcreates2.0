daf3a62420a6252a8b6f732404ca88a48090261d
Grant <gmontgomery1994@gmail.com>
2020-08-11 17:41:32 -0700

    post request works to lambda function, but there is an issue with return value and it not accepting json.

--- src/server/server.js
+++ src/server/server.js
@@ -21,7 +21,6 @@
 // )
 
 // app.use(bodyParser.urlencoded({ extended: false }))
-// app.use(bodyParser.json())
 
 // app.use(cors())
 // app.get("/send", (req, res) => {
@@ -80,76 +79,16 @@
 
 // app.listen(5000, () => console.log("Server started on port 5000"))
 
-const express = require("express")
-const bodyParser = require("body-parser")
-const cors = require("cors")
-require(`dotenv`).config()
+exports.handler = function (event, context, callback) {
+  console.log(event.body)
 
-const serverless = require("serverless-http")
-
-const app = express()
-
-const router = express.Router()
-
-const nodemailer = require("nodemailer")
-
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-
-router.post(
-  "/",
-  ({ body: { name, phone, email, subject, message, company } }, res) => {
-    const output = `
-  <p>A new visitor reached out!</p>
-  <h3>Message Details</h3>
-  <ul>
-  <li>Name: ${name} </li>
-  <li>Company: ${company} </li>
-  <li>Phone: ${phone} </li>
-  <li>Email: ${email} </li>
-  <li>Subject: ${subject} </li>
-  <li>Message: ${message} </li>
-  </ul>
-  `
-
-    // `${process.env.GATSBY_EMAIL_SENDER_PASSWORD}`
-    // create reusable transporter object using the default SMTP transport
-    let transporter = nodemailer.createTransport({
-      service: `gmail`,
-      host: `smtp.gmail.com`,
-      port: 587,
-      secure: false, // true for 465, false for other ports
-      auth: {
-        user: `${process.env.GATSBY_EMAIL_SENDER}`, // generated ethereal user
-        pass: `${process.env.GATSBY_EMAIL_SENDER_PASSWORD}`, // fake password.
-      },
-      tls: {
-        rejectUnauthorized: false,
-      },
-    })
-
-    console.log(body)
-    console.log(res)
-
-    const mailoptions = {
-      from: `"Portfolio Visitor"<nodetest69@gmail.com>`,
-      to: `grant@grantcreates.com`,
-      subject: `Visitor Contact Request`,
-      text: `Hello World?`,
-      html: output,
-    }
-
-    transporter.sendMail(mailoptions, (error, info) => {
-      if (error) {
-        return console.log(error), res.send(error)
-      }
-
-      console.log(`Message sent`, info.messageId)
-      res.send(info)
-    })
-  }
-)
-
-app.use("/.netlify/functions/server", router)
-
-module.exports.handler = serverless(app)
+  callback(null, {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "Content-Type",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+    },
+    body: "Hello, World",
+  })
+}
