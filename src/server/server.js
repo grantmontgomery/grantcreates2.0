70cf7040b7fcfbaf5d6851f9eeda9fb24202abd1
Grant <gmontgomery1994@gmail.com>
2020-08-19 16:57:14 -0700

    aol email doesn't work.

--- src/server/server.js
+++ src/server/server.js
@@ -22,7 +22,7 @@ exports.handler = async (event, context) => {
     `
 
       const mailoptions = {
-        from: `"Portfolio Visitor"<nodetest69@gmail.com>`,
+        from: `"Portfolio Visitor"<nodetest94@aol.com>`,
         to: `grant@grantcreates.com`,
         subject: `Visitor Contact Request`,
         text: `Hello World?`,
@@ -30,22 +30,15 @@ exports.handler = async (event, context) => {
       }
 
       let transporter = nodemailer.createTransport({
-        service: `Gmail`,
-        host: `smtp.gmail.com`,
+        service: `AOL`,
+        host: `smtp.aol.com`,
         sendMail: true,
         port: 587,
         requireTLS: true,
         secure: false, // true for 465, false for other ports
         auth: {
-          type: `OAuth2`,
-          xoauth2: xoauth2.createXOAuth2Generator({
-            user: `${process.env.GATSBY_EMAIL_SENDER}`,
-            pass: `${process.env.GATSBY_EMAIL_PASSWORD}`,
-            clientId: `458994737075-082sqo1dsai5f3skben153rsmh6k0j2d.apps.googleusercontent.com`,
-            clientSecret: `${process.env.GATSBY_CLIENT_SECRET}`,
-            refreshToken: `1//04qf1RFNW32a-CgYIARAAGAQSNwF-L9IrUlxk_NjLgHCgjj5R5RZl9t__mUIDb0039iFWFaHBEPBvbON_AfgysrIfGMaEhKz6eY0`,
-            accessToken: `ya29.a0AfH6SMBDZuUE31tTENb2tFyLtXcbLqzLPL22aB3nCNg3w6gEuo0KltjuR1PZN4CDYICi4W0IrqgsaFxffyboEZoEiLS3wiu8DZAbXAG0ye9LoHX5H5_BsFUoEgBwR61q3J2I2nNYbngKiXKxjZ3oVuaMRdJdUto29zU`,
-          }),
+          user: `${process.env.GATSBY_EMAIL_SENDER}`,
+          pass: `${process.env.GATSBY_EMAIL_PASSWORD}`,
         },
         tls: {
           rejectUnauthorized: false,
