4ed8812d6b85d7c1b1b451338e085a7acc056135
Grant <gmontgomery1994@gmail.com>
2020-08-19 15:43:42 -0700

    trying oauth on nodemailer.

--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,7 @@
 require(`dotenv`).config()
 
 const nodemailer = require("nodemailer")
+const xoauth2 = require("xoauth2")
 
 exports.handler = async (event, context) => {
   try {
@@ -29,15 +30,22 @@ exports.handler = async (event, context) => {
       }
 
       let transporter = nodemailer.createTransport({
-        service: `gmail`,
+        service: `Gmail`,
         host: `smtp.gmail.com`,
         sendMail: true,
         port: 587,
         requireTLS: true,
         secure: false, // true for 465, false for other ports
         auth: {
-          user: `${process.env.GATSBY_EMAIL_SENDER}`, // generated ethereal user
-          pass: `${process.env.GATSBY_EMAIL_SENDER_PASSWORD}`, // fake password.
+          type: `OAuth2`,
+          xoauth2: xoauth2.createXOAuth2Generator({
+            user: `${process.env.GATSBY_EMAIL_SENDER}`,
+            pass: `${process.env.GATSBY_EMAIL_PASSWORD}`,
+            clientId: `458994737075-082sqo1dsai5f3skben153rsmh6k0j2d.apps.googleusercontent.com`,
+            clientSecret: `${process.env.GATSBY_CLIENT_SECRET}`,
+            refreshToken: `1//04qf1RFNW32a-CgYIARAAGAQSNwF-L9IrUlxk_NjLgHCgjj5R5RZl9t__mUIDb0039iFWFaHBEPBvbON_AfgysrIfGMaEhKz6eY0`,
+            accessToken: `ya29.a0AfH6SMBDZuUE31tTENb2tFyLtXcbLqzLPL22aB3nCNg3w6gEuo0KltjuR1PZN4CDYICi4W0IrqgsaFxffyboEZoEiLS3wiu8DZAbXAG0ye9LoHX5H5_BsFUoEgBwR61q3J2I2nNYbngKiXKxjZ3oVuaMRdJdUto29zU`,
+          }),
         },
         tls: {
           rejectUnauthorized: false,
